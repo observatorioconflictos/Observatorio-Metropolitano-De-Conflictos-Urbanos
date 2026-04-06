e81df0a2a3ee1757cc9976568d9d6c228d05a51a
Observatorio Metropolitano de Desalojos y Conflictos Urbanos <observatorioconflictos@pucp.edu.pe>
2026-04-06 18:42:10 -0500

    Delete Observatorio Metropolitano de Conflictos Urbanos directory

--- Observatorio Metropolitano de Conflictos Urbanos/js/header-footer.js
+++
@@ -1,7 +0,0 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const menuToggle = document.getElementById('menu-toggle');
-    const navMenu = document.getElementById('nav-menu');
-    menuToggle.addEventListener('click', () => {
-        navMenu.classList.toggle('open');
-    });
-});
