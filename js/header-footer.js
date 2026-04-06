735139be3a92a34ecff77b2374c37f0c0a3cfa4f
Observatorio Metropolitano de Desalojos y Conflictos Urbanos <observatorioconflictos@pucp.edu.pe>
2026-04-06 18:43:34 -0500

    Add files via upload

---
+++ js/header-footer.js
@@ -0,0 +1,7 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const menuToggle = document.getElementById('menu-toggle');
+    const navMenu = document.getElementById('nav-menu');
+    menuToggle.addEventListener('click', () => {
+        navMenu.classList.toggle('open');
+    });
+});
